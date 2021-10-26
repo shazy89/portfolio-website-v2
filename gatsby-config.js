b28cbdc5255ee2b713b96be88e95fe164d92051c
Ed <erdo.shazy123@gmail.com>
2021-10-26 16:20:26 -0400

    searchbar logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,33 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Portfolio Project v2",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Portfolio Project v2',
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Erdoan Shaziman's portfolio webste`,
+        short_name: `Portfolio Website`,
+        start_url: `/`,
+        icon: `src/images/favicon-32x32.png`,
+        cache_busting_mode: 'none',
+        display: `standalone`,
+      },
     },
   ],
 };
